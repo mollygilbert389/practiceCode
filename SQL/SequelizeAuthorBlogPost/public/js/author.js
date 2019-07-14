6cb53c6e75b9e781bf390821ebf1f4cf377ce301
Molly Gilbert <msgilbert389@gmail.com>
2019-07-14 13:49:14 -0500

    need to learn about joining dbs

---
+++ SQL/SequelizeAuthorBlogPost/public/js/author.js
@@ -0,0 +1,82 @@
+$(document).ready(function() {
+
+  var nameInput = $("#author-name");
+  var authorList = $("tbody");
+  var authorContainer = $(".author-container");
+
+  $(document).on("submit", "#author-form", handleAuthorFormSubmit);
+  $(document).on("click", ".delete-author", handleDeleteButtonPress);
+
+  getAuthors();
+
+  function handleAuthorFormSubmit(event) {
+    event.preventDefault();
+
+    if (!nameInput.val().trim().trim()) {
+      return;
+    }
+
+    upsertAuthor({
+      name: nameInput
+        .val()
+        .trim()
+    });
+  }
+
+  function upsertAuthor(authorData) {
+    $.post("/api/authors", authorData)
+      .then(getAuthors);
+  }
+
+  function createAuthorRow(authorData) {
+    console.log(authorData);
+    var newTr = $("<tr>");
+    newTr.data("author", authorData);
+    newTr.append("<td>" + authorData.name + "</td>");
+    newTr.append("<td># of posts will display when we learn joins in the next activity!</td>");
+    newTr.append("<td><a href='/blog?author_id=" + authorData.id + "'>Go to Posts</a></td>");
+    newTr.append("<td><a href='/cms?author_id=" + authorData.id + "'>Create a Post</a></td>");
+    newTr.append("<td><a style='cursor:pointer;color:red' class='delete-author'>Delete Author</a></td>");
+    return newTr;
+  }
+
+  function getAuthors() {
+    $.get("/api/authors", function(data) {
+      var rowsToAdd = [];
+      for (var i = 0; i < data.length; i++) {
+        rowsToAdd.push(createAuthorRow(data[i]));
+      }
+      renderAuthorList(rowsToAdd);
+      nameInput.val("");
+    });
+  }
+
+  function renderAuthorList(rows) {
+    authorList.children().not(":last").remove();
+    authorContainer.children(".alert").remove();
+    if (rows.length) {
+      console.log(rows);
+      authorList.prepend(rows);
+    }
+    else {
+      renderEmpty();
+    }
+  }
+
+  function renderEmpty() {
+    var alertDiv = $("<div>");
+    alertDiv.addClass("alert alert-danger");
+    alertDiv.text("You must create an Author before you can create a Post.");
+    authorContainer.append(alertDiv);
+  }
+
+  function handleDeleteButtonPress() {
+    var listItemData = $(this).parent("td").parent("tr").data("author");
+    var id = listItemData.id;
+    $.ajax({
+      method: "DELETE",
+      url: "/api/authors/" + id
+    })
+      .then(getAuthors);
+  }
+});
